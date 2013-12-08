ca82d4636da99b38501a131b7242aa96b86e371f
Jeff Avallone <jeff.avallone@gmail.com>
2013-12-08 16:38:39 -0500

    Adding API docs

--- lib/token_session.rb
+++ lib/token_session.rb
@@ -1,3 +1,24 @@
+# Signed token session middleware
+#
+# This session middleware stores the session data in a string with a
+# cryptographic signature. This session manager is meant to be used for APIs,
+# and one of the Rack::Session middleware options is probably more appropriate
+# for most sites.
+#
+# Unlike typical session middlewares, the session content is not automatically
+# stored or sent to the user. It is the responsibility of the developer to send
+# the token to the client (by calling the {#to_s} method). The content of the
+# session is in no way hidden from the client (the token is just the session
+# data in JSON). It is however signed to prevent clients from forging a
+# session.
+#
+# By default, the client will have to send the session token back to the server
+# in the "X-Token" header.
+#
+# Other than the differences involving sending the session token to and from
+# the client, the session object itself is available from "rack.session" in the
+# request environment and acts like a Hash with a few methods added (see
+# {Session} for details).
 class TokenSession
 
   DEFAULT_OPTIONS = {
@@ -5,6 +26,19 @@ class TokenSession
     header: 'X-Token'
   }
 
+  # A new instance of TokenSession
+  #
+  # Other than the 'key' and 'header' options, all keyword options will be
+  # passed on to the {Session} class when it is instantiated.
+  #
+  # @param app [Object] Rack middleware or application
+  # @param options [Hash]
+  # @option options [String] :secret Required secret key used to sign tokens
+  # @option options [String] :key ('rack.session') Key to store session under
+  #   in environment
+  # @option options [String] :header ('X-Token') Request header to load token
+  #   from
+  # @raise [ArgumentError] if no secret is provided
   def initialize(app, options={})
     @app = app
 
@@ -18,16 +52,35 @@ class TokenSession
     end
   end
 
+  # Standard Rack middleware call method
+  #
+  # A {Session} instance will be created and stored in the provided key in the
+  # environment ("rack.session" by default). Otherwise, this functions like a
+  # typical Rack middleware call method.
+  #
+  # @param env [Hash] Rack request environment
   def call(env)
     @app.call(env.merge(@key => self.session(env)))
   end
 
+  # Create the {Session} instance for a request
+  #
+  # Creates the session from the provided request header. The session will be
+  # reset if it is not valid (see the {Session#valid?} and {Session.reset!}
+  # methods for more detail).
+  #
+  # @param env [Hash] Rack request environment
+  # @return [Session]
   def session(env)
     session = Session.new(self.token(env), @options)
     session.reset! unless session.valid?
     session
   end
 
+  # Retrieve the session token from request header
+  #
+  # @param env [Hash] Rack request environment
+  # @return [String]
   def token(env)
     env[@header]
   end
--- lib/token_session/session.rb
+++ lib/token_session/session.rb
@@ -2,6 +2,13 @@ require 'delegate'
 require 'openssl'
 require 'json'
 
+# Session object for the TokenSession
+#
+# This class delegates method calls to the Hash of session data. Any methods
+# that are available for a Hash will work on this session.
+#
+# This class should not be instantiated directly (though it can be). To make
+# use of a token session, add an instance of {TokenSession} to your middleware.
 class TokenSession::Session < ::SimpleDelegator
 
   SIGNATURE_KEY = :__sig
@@ -10,9 +17,16 @@ class TokenSession::Session < ::SimpleDelegator
     digest: 'sha1'
   }
 
-  def initialize(token_str=nil, options={})
+  # A new instance of Session
+  #
+  # @param token [String] Token from request headers
+  # @param options [Hash]
+  # @option options [String] :secret Required secret key used to sign tokens
+  # @option options [String] :digest OpenSSL digest algorithm name to use for
+  #   signing
+  def initialize(token=nil, options={})
     super begin
-      JSON.parse(token_str || '', symbolize_names: true)
+      JSON.parse(token || '', symbolize_names: true)
     rescue JSON::ParserError
       {}
     end
@@ -23,16 +37,25 @@ class TokenSession::Session < ::SimpleDelegator
     @signature = self.delete(self.class::SIGNATURE_KEY)
   end
 
+  # Clears all data from the session
   def reset!
     @signature = nil
     self.__setobj__({})
   end
 
+  # Generates the HMAC signature for the current session data
   def signature
     data = JSON.generate(self)
     OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new(@digest), @secret, data)
   end
 
+  # Check the validity of the session data
+  #
+  # Returns true if the session is signed and that signature is valid. A newly
+  # created session will be marked as invalid (since it was not created with a
+  # signature).
+  #
+  # @return [Boolean]
   def valid?
     if @signature.nil?
       return false
@@ -41,6 +64,14 @@ class TokenSession::Session < ::SimpleDelegator
     Rack::Utils.secure_compare(self.signature, @signature)
   end
 
+  # Convert the session data to a string
+  #
+  # This will also sign the session and include the signature in the generated
+  # JSON string.
+  #
+  # Use this method to send the session data to a client.
+  #
+  # @return [String]
   def to_s
     JSON.generate(self.merge(SIGNATURE_KEY => self.signature))
   end
